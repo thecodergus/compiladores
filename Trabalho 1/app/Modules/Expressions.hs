4672adb1f8cff2dea3fe88792c342c13ba873b53
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-04-22 21:33:43 -0300

    Ajustando umas paradas

--- Trabalho 1/app/Modules/Expressions.hs
+++ Trabalho 1/app/Modules/Expressions.hs
@@ -1,15 +1,23 @@
 -- Módulo Expressions.hs: Implementa os parsers para expressões.
 module Expressions where
 
-import Text.Parsec
-import Text.Parsec.Expr
-import Text.Parsec.Token
-import Text.Parsec.String (Parser)
 import Control.Monad (liftM)
 import Control.Monad.Identity (Identity)
-
-import Lexer
+import Lexer (lexer)
+import Text.Parsec (choice, try, (<?>))
+import Text.Parsec.Expr
+  ( Assoc (AssocLeft),
+    Operator (Infix, Prefix),
+    buildExpressionParser,
+  )
+import Text.Parsec.String (Parser)
+import Text.Parsec.Token
+  ( GenTokenParser (float, identifier, integer, parens, reservedOp),
+  )
 import Types
+  ( Expr (Const, IdVar, Neg, (:*:), (:+:), (:-:), (:/:)),
+    TCons (CDouble, CInt),
+  )
 
 -- Parser para expressões aritméticas.
 expr :: Parser Expr
@@ -17,25 +25,26 @@ expr = buildExpressionParser table factor <?> "expression"
 
 -- Tabela de operadores.
 table :: [[Operator String () Identity Expr]]
-table = [ 
-			[ prefix "-" Neg, prefix "+" id ],
-	        [ binary "*" (:*:) AssocLeft, binary "/" (:/:) AssocLeft ],
-	        [ binary "+" (:+:) AssocLeft, binary "-" (:-:) AssocLeft ]
-        ]
+table =
+  [ [prefix "-" Neg, prefix "+" id],
+    [binary "*" (:*:) AssocLeft, binary "/" (:/:) AssocLeft],
+    [binary "+" (:+:) AssocLeft, binary "-" (:-:) AssocLeft]
+  ]
 
 -- Funções auxiliares para a tabela de operadores.
 binary :: String -> (Expr -> Expr -> Expr) -> Assoc -> Operator String () Identity Expr
-binary name fun = Infix (do{ reservedOp lexer name; return fun })
+binary name fun = Infix (do reservedOp lexer name; return fun)
 
 prefix :: String -> (Expr -> Expr) -> Operator String () Identity Expr
-prefix name fun = Prefix (do{ reservedOp lexer name; return fun })
+prefix name fun = Prefix (do reservedOp lexer name; return fun)
 
 -- Parser para fatores em uma expressão.
 factor :: Parser Expr
-factor = choice [ 
-				try (parens lexer expr),
-                fmap IdVar (identifier lexer),
-                fmap (Const . CInt . fromInteger) (integer lexer),
-                fmap (Const . CDouble) (float lexer)
-                ]
-         <?> "factor"
+factor =
+  choice
+    [ try (parens lexer expr),
+      fmap IdVar (identifier lexer),
+      fmap (Const . CInt . fromInteger) (integer lexer),
+      fmap (Const . CDouble) (float lexer)
+    ]
+    <?> "factor"
--- Trabalho 1/app/Modules/Lexer.hs
+++ Trabalho 1/app/Modules/Lexer.hs
@@ -1,29 +1,46 @@
 -- Módulo Lexer.hs: Implementa os parsers para tokens básicos e palavras reservadas.
 module Lexer where
 
-import Text.Parsec
+import Text.Parsec (alphaNum, char, letter, oneOf, (<|>))
+import Text.Parsec.Language (LanguageDef, emptyDef)
 import Text.Parsec.String (Parser)
-import Text.Parsec.Language (emptyDef)
 import Text.Parsec.Token
-import Text.Parsec.Language
-
-import Types
+  ( GenLanguageDef
+      ( LanguageDef,
+        caseSensitive,
+        commentEnd,
+        commentLine,
+        commentStart,
+        identLetter,
+        identStart,
+        nestedComments,
+        opLetter,
+        opStart,
+        reservedNames,
+        reservedOpNames
+      ),
+    GenTokenParser (reserved),
+    LanguageDef,
+    TokenParser,
+    makeTokenParser,
+  )
+import Types (Type (..))
 
 -- Define a linguagem de programação específica.
 languageDef :: LanguageDef ()
-languageDef = LanguageDef
-    {	
-        commentStart = "/*",
-        commentEnd = "*/",
-        commentLine = "//",
-        nestedComments = True,
-        identStart = letter <|> char '_',
-        identLetter = alphaNum <|> char '_',
-        opStart = oneOf ":!#$%&*+./<=>?@\\^|-~",
-        opLetter = oneOf ":!#$%&*+./<=>?@\\^|-~",
-        reservedOpNames = ["+", "-", "*", "/", "==", "/=", "<", ">", "<=", ">=", "&&", "||", "!"],
-        reservedNames = ["int", "double", "string", "void", "return", "if", "else", "while", "print", "read"],
-        caseSensitive = True
+languageDef =
+  LanguageDef
+    { commentStart = "/*",
+      commentEnd = "*/",
+      commentLine = "#",
+      nestedComments = True,
+      identStart = letter <|> char '_',
+      identLetter = alphaNum <|> char '_',
+      opStart = oneOf ":!#$%&*+./<=>?@\\^|-~",
+      opLetter = oneOf ":!#$%&*+./<=>?@\\^|-~",
+      reservedOpNames = ["+", "-", "*", "/", "==", "/=", "<", ">", "<=", ">=", "&&", "||", "!"],
+      reservedNames = ["int", "double", "string", "void", "return", "if", "else", "while", "print", "read"],
+      caseSensitive = True
     }
 
 -- Cria um lexer com base na definição da linguagem.
@@ -33,7 +50,7 @@ lexer = makeTokenParser languageDef
 -- Parser para os tipos de dados suportados pela linguagem.
 typeParser :: Parser Type
 typeParser =
-    fmap (const TInt) (reserved lexer "int") <|>
-    fmap (const TDouble) (reserved lexer "double") <|>
-    fmap (const TString) (reserved lexer "string") <|>
-    fmap (const TVoid) (reserved lexer "void")
+  fmap (const TInt) (reserved lexer "int")
+    <|> fmap (const TDouble) (reserved lexer "double")
+    <|> fmap (const TString) (reserved lexer "string")
+    <|> fmap (const TVoid) (reserved lexer "void")
