926d3b03f3114a2b376b64516a3d3fdf0ae329f3
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-05-07 09:40:53 -0300

    Add modulo de Comando

--- Trabalho 1/app/Modules/BlocksAndCommandLists.hs
+++ Trabalho 1/app/Modules/BlocksAndCommandLists.hs
@@ -1,1 +1,14 @@
 module BlocksAndCommandLists where
+
+import Commands (command)
+import Lexer (braces', whiteSpace')
+import Text.Parsec (Parsec, many, try, (<|>))
+import Types (Bloco, Comando)
+
+-- Função principal para analisar blocos
+block :: Parsec String () Bloco
+block = braces' commandList
+
+-- Função auxiliar para analisar listas de comandos
+commandList :: Parsec String () [Comando]
+commandList = many (whiteSpace' *> command <* whiteSpace')
