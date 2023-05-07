94e0cdac0ba162186b272f0d448ba2b425f1cf18
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-05-07 09:56:27 -0300

    Fazendo funcionar mas o projeto não esta pronto

--- Trabalho 1/app/Modules/ProgramParser.hs
+++ Trabalho 1/app/Modules/ProgramParser.hs
@@ -1,1 +1,18 @@
 module ProgramParser where
+
+import BlocksAndCommandLists (block)
+import FunctionsAndParameters (functionDefinition)
+import Lexer (whiteSpace')
+import Text.Parsec (Parsec, eof, many, try, (<|>))
+import Types (Bloco, Comando, Funcao, Programa (..), Var)
+import VariableDeclarations (variableDeclarations)
+
+-- Função principal para analisar um programa completo
+programParser :: Parsec String () Programa
+programParser = do
+  whiteSpace'
+  funDeclarations <- many functionDefinition
+  varDeclarations <- variableDeclarations
+  mainBlock <- block
+  eof
+  return $ Prog funDeclarations [] varDeclarations mainBlock
