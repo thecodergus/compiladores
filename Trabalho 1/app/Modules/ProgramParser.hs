da795fedaffcc973f37c4a7548365508b1432665
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-05-14 16:53:52 -0300

    vou deixar como esta

--- Trabalho 1/app/Modules/ProgramParser.hs
+++ Trabalho 1/app/Modules/ProgramParser.hs
@@ -10,27 +10,24 @@ import Test.QuickCheck (Fun(Fun))
 import VariableDeclarations (variableDeclarations)
 import Data.Either (partitionEithers)
 import Data.Maybe (listToMaybe)
+import Commands (command)
 
 -- Função principal para analisar um programa completo
 programParser :: Parsec String () Programa
 programParser = do
-  whiteSpace' -- ignora espaços em branco
-  let varOrFun =
-        try (Left <$> functionDefinition)
-          <|> try (Right . Left <$> variableDeclarations)
-          <|> try (Right . Right . Left <$> parseFunctionsWithParamsAndVars)
-          <|> try (Right . Right . Right <$> block)
+      whiteSpace' -- ignora espaços em branco
+      let varOrFun = -- analisa declarações de variáveis ou funções, e definições de funções
+            try (Left . Left <$> variableDeclarations) -- analisa declarações de variáveis
+            <|> try (Left . Right <$> functionDefinition) -- analisa declarações de funções
+            <|> try (Right . Left <$> parseFunctionsWithParamsAndVars) -- analisa definições de funções
+            
+      declarations <- manyTill varOrFun (lookAhead eof) -- analisa declarações até o fim do arquivo
 
-  declarations <- manyTill varOrFun (lookAhead eof)
+      let (decVarsAndFuncs, rest) = partitionEithers declarations -- separa declarações de variáveis e funções
+      let (variableDeclarations, functionDeclarations) = partitionEithers decVarsAndFuncs -- separa declarações de variáveis e funções
+      let (funsWithParams, _) = partitionEithers rest -- separa definições de funções e comandos
 
-  let (funDeclarations, rest) = partitionEithers declarations
-  let (variableDeclarations, rest') = partitionEithers rest
-  let (funsWithParams, mainBlocks) = partitionEithers rest'
-
-  let mainBlock = case mainBlocks of
-        [] -> []
-        (b : _) -> b
-
-  whiteSpace' -- ignora espaços em branco
-  eof -- verifica se chegou ao fim do arquivo
-  return $ Prog funDeclarations (concat funsWithParams) (concat variableDeclarations) mainBlock
+      whiteSpace' -- ignora espaços em brancow
+      eof -- verifica se chegou ao fim do arquivo
+      
+      return $ Prog functionDeclarations (concat funsWithParams) (concat variableDeclarations) [] -- retorna o programa
