8cb6090b213653340774899a9e6a416d53977060
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-05-14 19:49:21 -0300

    Adicionando mais um exemplo

--- Trabalho 1/app/Main.hs
+++ Trabalho 1/app/Main.hs
@@ -13,3 +13,4 @@ main = do
   print $ parse programParser "" "void main(int a, int b); void main(int a, int b){int a; a = 2;} int b(){return 2;} int c(){return 2;}"
   print $ parse programParser "" "void main(int a, int b); void main(int a, int b){} int b(){return 2;} int c(){b(); return 2;}"
   print $ parse programParser "" "int main(int a, int b); void main(int a, int b){return b();}"
+  print $ parse programParser "" "int main(int a, int b); void main(int a, int b){a = b(); return b();}"
