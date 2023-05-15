b5bb3cd46845f6a0f0fb9774dfe48df2d6931032
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-05-15 17:20:15 -0300

    Adicionando leitura de arquivo para teste

--- Trabalho 1/app/Main.hs
+++ Trabalho 1/app/Main.hs
@@ -4,13 +4,19 @@ import Text.Parsec (parse)
 
 main :: IO ()
 main = do
-  print $ parse programParser "" "void main(int a, int b); void main(int a, int b){return 0;}"
-  print $ parse programParser "" "void main(int a, int b); void main(int a, int b){int a; a = 2;}"
-  print $ parse programParser "" "void main(int a, int b); void main(int a, int b){int a; a = 2;} int b(){return 2;} int c(){}"
-  print $ parse programParser "" "int A, B; void main(int a, int b); void main(int a, int b){int a; a = 2;} int b(){return 2;} int c(){}"
-  print $ parse programParser "" "int A, B; void main(){A = 2;}"
-  print $ parse programParser "" "int A, B; void main(){if(A == 2){A = 3;}}"
-  print $ parse programParser "" "void main(int a, int b); void main(int a, int b){int a; a = 2;} int b(){return 2;} int c(){return 2;}"
-  print $ parse programParser "" "void main(int a, int b); void main(int a, int b){} int b(){return 2;} int c(){b(); return 2;}"
-  print $ parse programParser "" "int main(int a, int b); void main(int a, int b){return b();}"
-  print $ parse programParser "" "int main(int a, int b); void main(int a, int b){a = b(); return b();}"
+  -- print $ parse programParser "" "void main(int a, int b); void main(int a, int b){return 0;}"
+  -- print $ parse programParser "" "void main(int a, int b); void main(int a, int b){int a; a = 2;}"
+  -- print $ parse programParser "" "void main(int a, int b); void main(int a, int b){int a; a = 2;} int b(){return 2;} int c(){}"
+  -- print $ parse programParser "" "int A, B; void main(int a, int b); void main(int a, int b){int a; a = 2;} int b(){return 2;} int c(){}"
+  -- print $ parse programParser "" "int A, B; void main(){A = 2;}"
+  -- print $ parse programParser "" "int A, B; void main(){if(A == 2){A = 3;}}"
+  -- print $ parse programParser "" "void main(int a, int b); void main(int a, int b){int a; a = 2;} int b(){return 2;} int c(){return 2;}"
+  -- print $ parse programParser "" "void main(int a, int b); void main(int a, int b){} int b(){return 2;} int c(){b(); return 2;}"
+  -- print $ parse programParser "" "int main(int a, int b); void main(int a, int b){return b();}"
+  -- print $ parse programParser "" "int main(int a, int b); void main(int a, int b){a = b(); return b();}"
+  print $ "Digite o caminho para o arquivo de teste: "
+  filePath <- getLine
+  input <- readFile filePath
+  case parse programParser "" input of
+    Left err -> print err
+    Right ast -> print ast
---
+++ arquivo.c
@@ -0,0 +1,11 @@
+int main();
+
+
+int main(){
+    int a, b;
+
+    a = 1;
+    b = 2;
+
+    return 0;
+}
