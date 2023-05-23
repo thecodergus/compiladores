7016d86a43293b3ebbdb1679b77e71ed49d9aed4
Gustavo Michels de Camargo <gustavo.camargo@strokmatic.com>
2023-05-23 20:06:13 -0300

    Tirando função sem sentido

--- Trabalho 1/app/Modules/ProgramParser.hs
+++ Trabalho 1/app/Modules/ProgramParser.hs
@@ -8,24 +8,11 @@ import Lexer (whiteSpace', reserved', parens')
 import Text.Parsec (Parsec, eof, many, try, (<|>), choice, manyTill, lookAhead, option)
 import Types (Bloco, Comando, Funcao, Programa (..), Var, Id)
 import Control.Monad (void, unless)
-import Test.QuickCheck (Fun(Fun))
 import VariableDeclarations (variableDeclarations)
 import Data.Either (partitionEithers)
 import Data.Maybe (listToMaybe)
 import BlocksAndCommands (command, block)
 import Data.Foldable (find)
-
--- Função auxiliar para análise comum
-{-
-Função 'parseCommon' recebe uma função de análise 'p' e retorna outra função de análise.
-A função de análise retornada vai aplicar a função de análise 'p' várias vezes até chegar ao fim do arquivo.
-'manyTill p (lookAhead eof)' significa "aplique a função de análise 'p' repetidamente até que o fim do arquivo seja avistado".
-'lookAhead eof' é uma função de análise que tem sucesso se o fim do arquivo foi alcançado, mas sem consumir nenhum caractere.
-Isso significa que 'parseCommon p' pode ser usado para analisar várias instâncias de algo até o fim do arquivo, sem consumir o fim do arquivo.
-Por exemplo, 'parseCommon variableDeclarations' irá analisar várias declarações de variáveis até o fim do arquivo.
--}
-parseCommon :: Parsec String () a -> Parsec String () [a]
-parseCommon p = manyTill p (lookAhead eof)
 
 -- Função para analisar e verificar o final do arquivo
 parseAndCheckEOF :: Parsec String () a -> Parsec String () a
@@ -46,7 +33,7 @@ programParser = do
           <|> try (Left . Right <$> functionDefinition) -- analisa declarações de funções
           <|> try (Right . Left <$> parseFunctionsWithParamsAndVars) -- analisa definições de funções
   
-  declarations <- parseCommon varOrFun -- analisa declarações de variáveis, definições de funções e comandos
+  declarations <- manyTill varOrFun (lookAhead eof) -- analisa declarações de variáveis, definições de funções e comandos
 
   -- separa declarações de variáveis, definições de funções e comandos
   let (decVarsAndFuncs, rest) = partitionEithers declarations -- separa declarações de variáveis e funções
