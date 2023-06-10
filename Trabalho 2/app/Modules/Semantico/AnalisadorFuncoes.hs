24c4f98447a00d0752db041ceeb38c7416532f24
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-06-10 20:48:56 -0300

    Criando o modulo para checar funções

--- Trabalho 2/app/Modules/Semantico/AnalisadorFuncoes.hs
+++ Trabalho 2/app/Modules/Semantico/AnalisadorFuncoes.hs
@@ -1,1 +1,46 @@
 module Semantico.AnalisadorFuncoes where
+import Sintatico.Types (Id, Type, Funcao ((:->:)), Expr (Chamada), Var ((:#:)))
+import Semantico.ErrosSemantico (ErroSemantico (..))
+import Semantico.AvisosSemantico (AvisoSemantico (..))
+import Data.Foldable (find)
+import Semantico.AnalisadorVariaveis (inferirTipo)
+import Data.List (sortOn, groupBy)
+
+-- | Analisa uma chamada de função para verificar se ela foi declarada, 
+-- | se a quantidade de parâmetros é correta e se os tipos de parâmetros são compatíveis.
+analisarChamadaFuncao :: [Funcao] -> [Var] -> Expr -> ([ErroSemantico], [AvisoSemantico])
+analisarChamadaFuncao funcoes vars (Chamada id exprs) =
+  case encontrarFuncao funcoes id of
+    Just (_ :->: (params, _)) ->
+      let paramTipos = tiposParametros params
+          argTipos = map (inferirTipo vars) exprs
+      in analisarParametros id paramTipos argTipos
+    Nothing -> ([ChamadaFuncaoNaoDeclarada id], [])
+  where
+    -- | Obtém os tipos dos parâmetros de uma função
+    tiposParametros = map (\(_ :#: tipo) -> tipo)
+    -- | Procura uma função pelo identificador em uma lista de funções
+    encontrarFuncao funcoes id = find (\(fid :->: _) -> fid == id) funcoes
+
+-- | Compara o número de parâmetros e os tipos de uma chamada de função com a declaração
+analisarParametros :: Id -> [Type] -> [Type] -> ([ErroSemantico], [AvisoSemantico])
+analisarParametros id paramTipos argTipos
+  | length paramTipos /= length argTipos = ([NumeroParametrosIncorreto id], [])
+  | paramTipos == argTipos = ([], [])
+  | otherwise = ([TiposParametrosIncompativel id], [])
+
+-- | Analisa uma lista de declarações de funções para identificar funções declaradas várias vezes
+analisarDeclaracoesFuncoes :: [Funcao] -> [ErroSemantico]
+analisarDeclaracoesFuncoes funcoes =
+  let duplicatas = encontrarDuplicatas funcoes
+  in map funcaoMultiplamenteDeclarada duplicatas
+  where
+    -- | Cria um erro de função declarada várias vezes para um grupo de funções com o mesmo nome
+    funcaoMultiplamenteDeclarada grupo = FuncaoMultiplamenteDeclarada (getId $ head grupo)
+    -- | Obtém o identificador de uma função
+    getId (fid :->: _) = fid
+    -- | Encontra grupos de funções com o mesmo nome
+    encontrarDuplicatas funcoes =
+      let sortedFuncoes = sortOn (\(fid :->: _) -> fid) funcoes
+          grupos = groupBy (\(fid1 :->: _) (fid2 :->: _) -> fid1 == fid2) sortedFuncoes
+      in filter ((> 1) . length) grupos
