45b28fde41181e00a5917efdb686f194424cd1a2
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-06-11 14:36:53 -0300

    Começando a criar o programa principal

--- Trabalho 2/app/Modules/Semantico/AnalisadorPrincipal.hs
+++ Trabalho 2/app/Modules/Semantico/AnalisadorPrincipal.hs
@@ -1,1 +1,14 @@
 module Semantico.AnalisadorPrincipal where
+
+import Sintatico.Types ( Funcao, FuncaoBloco, Programa(..), Var )
+import Semantico.ErrosSemantico ( ErroSemantico (ChamadaFuncaoNaoDeclarada) )
+import Semantico.AvisosSemantico ( AvisoSemantico )
+import Semantico.AnalisadorVariaveis (analisarVariaveis)
+import Semantico.AnalisadorFuncoes (analisarDeclaracoesFuncoes, encontrarFuncao, analisarChamadaFuncao)
+import Semantico.AnalisadorComandos (analisarComandos)
+
+-- | Função principal que analisa um programa
+analisarPrograma :: Programa -> ([ErroSemantico], [AvisoSemantico])
+analisarPrograma (Prog funcoes funcoesBlocos vars bloco) =
+    -- Analisar bloco principal
+    analisarComandos vars bloco
