8e9e7b1d4e2add9e96026fc7ff9802d8bb54cf99
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-06-18 19:10:03 -0300

    Ajustando propagação de mudanças

--- Trabalho 2/app/Modules/Semantico/AnalisadorPrincipal.hs
+++ Trabalho 2/app/Modules/Semantico/AnalisadorPrincipal.hs
@@ -8,6 +8,16 @@ import Semantico.AnalisadorFuncoes (analisarDeclaracoesFuncoes, encontrarFuncao,
 import Semantico.AnalisadorComandos (analisarComandos)
 
 -- | Função principal que analisa um programa
-analisarPrograma :: Programa -> ([ErroSemantico], [AvisoSemantico])
-analisarPrograma (Prog funcoes funcoesBlocos vars bloco) = do
-    analisarComandos vars bloco
+analisarPrograma :: Programa -> ([ErroSemantico], [AvisoSemantico], Programa)
+analisarPrograma (Prog funcoes funcoesBlocos vars bloco) =
+  let -- Analisar o bloco principal
+      (errosBloco, avisosBloco, blocoModificado) = analisarComandos vars bloco
+
+      -- Aqui você pode adicionar código para analisar as funções e seus blocos,
+      -- por exemplo, percorrendo cada função e seu bloco correspondente.
+      -- (errosFuncoes, avisosFuncoes, funcoesModificadas) = ...
+
+      -- Combinar erros e avisos de blocos e funções
+      erros = errosBloco -- ++ errosFuncoes (se você analisou funções)
+      avisos = avisosBloco -- ++ avisosFuncoes (se você analisou funções)
+   in (erros, avisos, Prog funcoes funcoesBlocos vars blocoModificado)
